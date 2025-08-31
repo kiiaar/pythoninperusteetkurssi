b0bf66bc60ea193514a25e393869c9591af5fdf8
kiiaar <kiia.arokanto@hmail.com>
2025-08-31 16:59:17 +0300

    moduuli2 tehtävät 1 ja 2

---
+++ tehtavat/teht.2.2.py
@@ -0,0 +1,11 @@
+import math
+säde= int(input("Hei anna ympyrän säde: "))
+
+#muuttuja jossa lasku suoritetaan
+A = math.pi * säde
+
+print("Ympyrän pinta-ala on: ", A)
+
+#muotoiltu kahteen desimaaliin
+print(f"Ympyrän pinta-ala on: {A:.2f}")
+
