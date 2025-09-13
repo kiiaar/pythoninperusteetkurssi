34cc3377475cb78754a683398b40b51c0c6c7dde
kiiaar <kiia.arokanto@hmail.com>
2025-09-13 11:59:33 +0300

    korjauksia

--- tehtavat/teht3.4.py
+++ tehtavat/teht3.4.py
@@ -1,7 +1,7 @@
 vuosiluku = int(input("Kerro jokin vuosiluku, niin kerron sinulle onko se karkausvuosi: "))
 if vuosiluku % 100 == 0 and vuosiluku % 400 == 0:
-    print("Vuosi on karkausvuosi")
-elif vuosiluku % 4 == 0:
-    print("vuosi on karkausvuosi")
+    print("Vuosi on karkausvuosi!")
+elif vuosiluku % 4 == 0 and vuosiluku % 100 != 0:
+    print("Vuosi on karkausvuosi!")
 else:
-    print("vuosi ei ole karkausvuosi")
+    print("Vuosi ei ole karkausvuosi!")
